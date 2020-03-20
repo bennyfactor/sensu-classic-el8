ad6ae2e29f07a58855d235076e4eb79c886fcef5
Benjamin Lamb <bennyfactor@iaatb.net>
2020-03-20 11:52:21 -0400

    escape find command better in sensu-gem

--- config/software/sensu-gem.rb
+++ config/software/sensu-gem.rb
@@ -15,11 +15,10 @@ build do
 
   files_dir = "#{project.files_path}/#{name}"
 
-
   command "gem fetch sensu -v #{version}", env: env
 
   command "gem unpack sensu-#{version}.gem", env: env
-  command "LC_CTYPE=C && LANG=C && find ./sensu-#{version} -type f -exec sed -i '' 's/1.0.9.1/1.2.0.1/g' {} \;", env: env
+  command "LC_CTYPE=C && LANG=C && find ./sensu-#{version} -type f -exec sed -i '' 's/1.0.9.1/1.2.0.1/g' {} \\\;", env: env
   command "cd sensu-#{version}&& gem build sensu.gemspec", env: env
 
 
